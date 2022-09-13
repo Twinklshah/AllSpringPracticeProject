668d6d53d7c8cc2d240a257ccb1725a3d0cce412
Twinkle Shah <41550937+Twinklshah@users.noreply.github.com>
2022-09-13 17:58:45 +0530

    swagger
    swagger

--- Department/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ Department/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -14,12 +14,14 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer {
+@Configuration   // so spring boot will uinderstand that this file is a configuration where all properties are set 
+                 //configurattion file is njust like a application properties
+
+@EnableSwagger2   // used to enable the swagger on the application
+public class SwaggerConfig {
 	
 	
-	@Bean
+	@Bean         //create an object
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.example.demo"))
 			.paths(PathSelectors.regex("/.*")).build().apiInfo(apiEndPointsInfo());
@@ -28,7 +30,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
 	private ApiInfo apiEndPointsInfo() {
 		return new ApiInfoBuilder().title("Department Services ").description(
 				"Elearning is an android app  used by the Education team for assessment of VMware customer's employees skill level with the intent of recommending specific VMware courses according to the customer outcomes, goals and each employees skill level need in their roles.\n")
-				.contact(new Contact("Ajay Gaikwad", "www.ajaygaikwad.in", "ajay.gaikwad@newgen.co.in"))
+				.contact(new Contact("Twinkle Shah", "www.twinkle.in", "twinkle.shah@nimap.co.in"))
 				.license("Apache 2.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").version("1.0.0")
 				.build();
 	}	
